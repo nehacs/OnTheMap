a5ae1c0efdb33b62fe914aae612a06d31c1b56a7
Neha Agarwal <neha.cs@gmail.com>
2016-01-18 23:44:48 -0800

    Added information posted view. Geocoding not yet done

--- OnTheMap/PinPostingViewController.swift
+++ OnTheMap/PinPostingViewController.swift
@@ -7,8 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
 class PinPostingViewController: UIViewController {
+    
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    @IBOutlet weak var text3: UITextField!
+    
+    @IBOutlet weak var enterUrlTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextView!
+    @IBOutlet weak var findOnMapButton: UIButton!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var submitButton: UIButton!
     
     @IBAction func cancelAction(sender: AnyObject) {
         let tabViews = storyboard!.instantiateViewControllerWithIdentifier("MapTabBarController") as! UITabBarController
@@ -16,5 +28,17 @@ class PinPostingViewController: UIViewController {
     }
     
     @IBAction func findOnMapAction(sender: AnyObject) {
+        text1.hidden = true
+        text2.hidden = true
+        text3.hidden = true
+        locationTextField.hidden = true
+        findOnMapButton.hidden = true
+        
+        enterUrlTextField.hidden = false
+        mapView.hidden = false
+        submitButton.hidden = false
+    }
+    
+    @IBAction func submitAction(sender: AnyObject) {
     }
 }
